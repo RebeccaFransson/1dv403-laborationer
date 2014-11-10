1ea0c96bab48022af5d709a9a090ecec8bd99516
rf222cz <rf222cz@student.lnu.se>
2014-11-10 17:59:13 +0100

    Klar

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -3,17 +3,52 @@
 window.onload = function(){
 
 	
-	var birthday = function(date){
+	var birthday = function(date){ //om ej formen ÅÅÅÅ-MM-DD kasta undantag
+
+		if(!isNaN(new Date(date).getTime())) {
+
+			var now = new Date();
+			var birth = new Date(date);
+			
+			console.log(date);
+			console.log(birth - now);
+			console.log(birth.getFullYear());
+			console.log("now = " + now.getDay() + " birth = " + birth.getDay());
+			if(now < birth){
+				birth.setFullYear(now.getFullYear());//För att uträkningen ska fungera så sätter jag birth-året till året dagens datum har, på så sätt kan vi räkna med mån och dag.
+			}	
+			else if (now > birth){ 
+				birth.setFullYear(now.getFullYear()+1);
+			}
+				var numberOfDays = Math.ceil((birth - now)/(1000*60*60*24)); //skillnaden
+			console.log("dagar tills"+numberOfDays);
+			console.log("dag idag"+now.getDay());
+			console.log("dag födelse"+birth.getDay());
+			
+			if(numberOfDays === 365){
+					return 0;
+				}
+			if(numberOfDays > 365){
+				return (numberOfDays-356);
+			}
+			else{
+				return (numberOfDays);
+			}
+
+
+		}
+		else{
+			throw new Error("Du måste skriva ÅÅÅÅ-MM-DD, prova igen!")
+		}
 		
-
-
 			// Din kod här.
-
-
-
-
 	};
 	// ------------------------------------------------------------------------------
+//if(now <= birth){
+//				console.log("här blir birth year = now year+1");
+//				birth.setFullYear(now.getFullYear()); 
+//				console.log(now.getDay());
+//			}
 
 
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
