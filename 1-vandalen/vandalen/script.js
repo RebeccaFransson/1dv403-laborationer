aae3b3dadbc8c686a99a72552171e76a6f3c844c
rf222cz <rf222cz@student.lnu.se>
2014-11-11 12:38:17 +0100

    Planerat

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,6 +1,21 @@
 "use strict";
 
 var makePerson = function(persArr){
+	var info = [{name: "Rebecca Fransson", age: 20}, {name: "Richard Söderman", age: 67}, {name: "Caroline Forslund", age: 31}];
+
+	function minAge(age){
+		//sortera och ta ut lägsta värdet
+	}
+	function maxAge(age){
+		//sortera och ta ut högsta
+	}
+	function avarageAge(age){
+		//maxAge - minAge
+	}
+	function names(name){
+		//sort i bokstavsordning sepererade med ","
+	}
+
 
 
 	// Din kod här...
