64c057564857ea85d01259c739957e33c6a90ead
Rebecca Fransson <rf222cz@student.lnu.se>
2014-11-26 14:44:44 +0000

    gjort nagra message-functioner

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -10,24 +10,43 @@ var messageBoard = {//här ska vara en metod som kommer trigga igång de andra m
 
     messages: ['med1', 'med2'],
 
-    init: function(e){
+    init: function(){//privat function
+        var click = document.getElementById("textvalue"); //texten
+        var submit = document.getElementById("send"); //knappen
         
-        
-        
-   
-        var mess = new Message("meddelandet", new Date()); //skickar värden till konstruktor som skapar nytt objekt
-        
-        console.log("konstrukot m. meddelande och tid: "+mess);
+        submit.addEventListener("click", function(a){//när vi klickar på submit hoppar vi 
+        messageBoard.outputMessage(a);//till outputMessage funktionenegenskapen 
+        })
+         //skapa nytt objekt med text och datum
+         
+         
+        /*console.log("konstrukot m. meddelande och tid: "+mess);
         console.log("meddelandet: "+mess.getText());
         mess.setText("byter text");
         console.log("Nytt meddelande: "+mess);
         console.log("datum: "+mess.getDate().getHours()+":"+ mess.getDate().getMinutes()+":"+ mess.getDate().getSeconds());
+        */
+    },
         
-
+    outputMessage: function(a){
+        a.preventDefault(); //formuläret skickas inte vidare.
+        messageBoard.messages.push(new Message("meddelandet", new Date())) // Läser in texten från textrutan och skickar till funktionen "newObjekt"
+        
+    },
+     newObjekt: function(){
+        
+    },
+    
+    makeMessage: function(){//skapa nytt meddelande objekt när send knapp klickad på.
+        
+        
     }
+    
+   
+    
+    
 };
     //console.log(messageBoard.messages[1].getText());
-
 window.onload = messageBoard.init;
 
 
