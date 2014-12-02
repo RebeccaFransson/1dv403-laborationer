459767eb2f5e6d2ea6eb40131572b0397cde1d5b
Rebecca Fransson <rf222cz@student.lnu.se>
2014-12-02 14:32:19 +0000

    Kan flippa, men fungerar ej hela vagen

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var Memory = {
     
@@ -8,52 +8,67 @@ var Memory = {
         var row = 4;
         var col = 4;
         Memory.imgRandom.push(RandomGenerator.getPictureArray(row, col));
-
-
-        Memory.tableCeate(row, col, Memory.imgRandom); //skickar värderna över hur lång den skall vara till table-func
-       
-        console.log("slumpade nummer "+Memory.imgRandom);
+        
+        Memory.tableCreate(row, col, Memory.imgRandom); //skickar värderna över hur lång den skall vara till table-func
+        Memory.click(array, count);
+        console.log('slumpade nummer '+Memory.imgRandom);
         
     },
 
-    tableCeate: function(c, r, array){
+    tableCreate: function(c, r, array){
         //body reference 
-        var cards = document.querySelector("#cards");
+        var cards = document.querySelector('#cards');
 
         // create elements <table> and a <tbody>
-        var tbl     = document.createElement("table");
-        var tblBody = document.createElement("tbody");
+        var tbl     = document.createElement('table');
+        var tblBody = document.createElement('tbody');
 
         // cells creation
         for (var j = 0; j < c; j++) {
             // table row creation
-            var row = document.createElement("tr");
+            var row = document.createElement('tr');
+            //.setAttribute('class', j) // Göra ny klass så att klickmetoden fungerar?
 
             for (var i = 0; i < r; i++) {
                 // create element <td> and text node 
                 //Make text node the contents of <td> element
                 // put <td> at end of the table row
-                var cell = document.createElement("td");   
-                var img = document.createElement("img");
+                var cell = document.createElement('td');   
+                var img = document.createElement('img');
                 var count = ((j * r) + i);
-                var src = "pics/"+array[0][count]+".png";
-                img.setAttribute("src", src);
-                 
-                 var cellImg = img;
+                
+                var src = 'pics/'+array[0][count]+'.png';//dimentionella arraysns nr
+                img.setAttribute('src', 'pics/0.png');
+                img.setAttribute('alt', ''+array[0][count]+'');
 
-                cell.appendChild(cellImg);
+                cell.appendChild(img);
                 row.appendChild(cell);
             }
-
             //row added to end of table body
             tblBody.appendChild(row);
         }
-
         // append the <tbody> inside the <table>
         tbl.appendChild(tblBody);
         // put <table> in the <body>
         cards.appendChild(tbl);
         
+        return array, count;
+    },
+    
+    click: function(array, count){
+        var src = 'pics/'+array[0][count]+'.png';//dimentionella arraysns nr
+        var cards = document.querySelector('table');
+        cards.addEventListener('click', function(e){
+           var klickedNode = e.target;
+           //loopar alla ifsatserna
+           for(var i = 0; i <= 8; i++){
+                if(klickedNode.getAttribute('alt') == i){
+                klickedNode.setAttribute('src', src);//Ger bilden en annan source och därför en annan bild
+           }
+           }
+           
+           
+        });
     },
     
     
