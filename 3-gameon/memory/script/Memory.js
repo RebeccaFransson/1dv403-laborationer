425f18878c03fccc7731b34265c7bf95f2b9411f
Rebecca Fransson <rf222cz@student.lnu.se>
2014-12-03 14:04:38 +0000

    Tror jag gjort fel pa hela timern...

--- 3-gameon/memory/script/Memory.js
+++ 3-gameon/memory/script/Memory.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//https://www.youtube.com/watch?v=c_ohDPWmsM0
 var Memory = {
     
     imgRandom: [],
@@ -8,83 +8,108 @@ var Memory = {
     go: function(){
         var row = 4;
         var col = 4;
+        
+        
         Memory.imgRandom.push(RandomGenerator.getPictureArray(row, col));
         
-        Memory.tableCreate(row, col, Memory.imgRandom); //skickar värderna över hur lång den skall vara till table-func
-        Memory.click(Memory.imgRandom);
-
+        Memory.tableCreate(row, col); //skickar värderna över hur lång den skall vara till table-func
+        Memory.click();
 
         console.log('slumpade nummer '+Memory.imgRandom);
         
     },
 
-    tableCreate: function(c, r, array){
-        //body reference 
+    tableCreate: function(c, r){
         var cards = document.querySelector('#cards');
 
-        // create elements <table> and a <tbody>
         var tbl     = document.createElement('table');
         var tblBody = document.createElement('tbody');
 
-        // cells creation
         for (var j = 0; j < c; j++) {
-            // table row creation
             var row = document.createElement('tr');
             //.setAttribute('class', j) // Göra ny klass så att klickmetoden fungerar?
 
             for (var i = 0; i < r; i++) {
-                // create element <td> and text node 
-                //Make text node the contents of <td> element
-                // put <td> at end of the table row
                 var cell = document.createElement('td');   
                 var img = document.createElement('img');
                 var count = ((j * r) + i);
                 
                 img.setAttribute('src', 'pics/0.png');
-                img.setAttribute('class', ''+array[0][count]+'');
+                img.setAttribute('class', ''+Memory.imgRandom[0][count]+'');
 
                 cell.appendChild(img);
                 row.appendChild(cell)
                 
                 
             }
-            //row added to end of table body
             tblBody.appendChild(row);
             
         }
-        // append the <tbody> inside the <table>
         tbl.appendChild(tblBody);
-        // put <table> in the <body>
         cards.appendChild(tbl);
         
        
     },
+    che: undefined,
+    timeout: undefined,
+    score: 0,
 //KLICK FUNKTIONER
-    click: function(array){
+    click: function(){
+        var times = 0;
         var cards = document.querySelector('table');
         cards.addEventListener('click', function(e){
-            Memory.flip(array, e);
+                //times++;         //plussar på antal ggr gissade
+            if(times < 2 ){
+                console.log("times är "+times);
+                Memory.flip(e);
+                Memory.write();
+                times++;
+            }else {
+                setTimeout(function() {
+                    times = 0;
+                    Memory.che = 0;
+                }, 2000);
+            }
+            
+            
         });
+        
     },
     
-    flip: function(array, e){
+    flip: function(e){
         
-           for(var i = 0; i <= array[0].length; i++){
+           for(var i = 0; i <= Memory.imgRandom[0].length; i++){ //loopar alla ifsatserna
                 var src = 'pics/'+i+'.png';//dimentionella arraysns nr 
                 var klickedNode = e.target;
                 
-                //loopar alla ifsatserna
+
+                
                 if(klickedNode.getAttribute('class') == i){ //Om bildens klass har det slumpadenummret
+                    
                     klickedNode.setAttribute('src', src);//Ger bilden en annan source och därför en annan bild
-                    setTimeout(Memory.timer, 1000, klickedNode);
+                    
+                    console.log("che = " + Memory.che + " i = " + i);
+                    if(Memory.che == i){
+                        Memory.score++;
+                        clearTimeout(Memory.timeout);
+                    }else{
+                        Memory.timeout = setTimeout(Memory.timer, 1000, klickedNode);
+                    }
+                    Memory.che = i;
                 }
                 
            }
     },
     timer: function(klickedNode){
         klickedNode.setAttribute('src', 'pics/0.png');//bilden återställs efter en sek
+    },
+    write: function(){
+        var points = document.querySelector('#points');
+        var p = document.querySelector('p');
+        p.innerHTML = "Poäng "+(Memory.score);
+        
     }
-    
+   
     
     
 };
