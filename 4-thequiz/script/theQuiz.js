e4baa1e971d4e689773cf375b2cee2b421ac8bb6
Rebecca Fransson <rf222cz@student.lnu.se>
2014-12-12 14:56:10 +0000

    Klar! Rasums programmeringsstuga var awesome!

--- 4-thequiz/script/theQuiz.js
+++ 4-thequiz/script/theQuiz.js
@@ -2,48 +2,50 @@
 var theQuiz = {
 
 nextURL: '',
+times: 0,
+timedArr: [],
+
     get: function(){
-        console.log('innan skall vara undefined '+theQuiz.nextURL);
-        //  if(theQuiz.nextURL != null){
-        //      console.log('next url finns'+theQuiz.nextURL);
-        //      theQuiz.nextURL = theQuiz.nextURL;
-        //  }else{
-        //      console.log('next url finns INTE');
-        //      theQuiz.nextURL = "http://vhost3.lnu.se:20080/question/1";
-        //  }
+        
+        if(theQuiz.times != 0){
+            theQuiz.timedArr.push(theQuiz.times);
+            console.log('ARRAYN!! '+theQuiz.timedArr);
+            theQuiz.times = 0;
+        }
+        
+        //theQuiz.timedArr.length = 0;
+        
         theQuiz.nextURL = theQuiz.nextURL || "http://vhost3.lnu.se:20080/question/1";
-        console.log('nextURL: '+theQuiz.nextURL);
             var xhr = new XMLHttpRequest();
             
             xhr.onreadystatechange = function(){ //denna kod skall köras när vi får ett svar
-                console.log('svar från server');
                 if(xhr.readyState === 4 && xhr.status === 200){//allt ok
                     
                     var quest = JSON.parse(xhr.responseText);
-                    console.log('OK '+quest.question); //svarstext
                     
                     document.getElementById('quest').innerHTML = quest.question;
-                    
-                    document.getElementById('send').addEventListener('click', function(e){//hämtar ut knapp, sätter klick
+                    document.getElementById('send').onclick = function(e){//hämtar ut knapp, sätter klick
+                    theQuiz.times++;
+                    console.log('FÖRSÖK FÖR DENNA FRÅGA ÄR: '+theQuiz.times);
                     e.preventDefault();
                     theQuiz.send(xhr, quest);
-                });
+                };
                 
                 }else if(xhr.status === 404){
                         console.log('Hittades ej');
+                }else{
+                    console.log('Annat problem med att ta emot från server');
                 }
             };
             
             xhr.open('GET', theQuiz.nextURL, true); //asynkromt anrop
             
             xhr.send(null);
-        
-            
     },
     
     send: function(xhr, quest){
             var input = document.getElementById('textvalue').value;
-                    
+            
             xhr.open('POST', quest.nextURL, true);//skicka data till nästa url
             xhr.setRequestHeader('Content-Type', 'application/json');//talar om att ypen är json
             
@@ -55,28 +57,32 @@ nextURL: '',
             xhr.send(JSON.stringify(product));
             
             xhr.onreadystatechange = function(){ //denna kod skall köras när vi får ett svar
-                console.log('svar: '+input);
-                var ansBox = document.getElementById('answer');
-                var answer = JSON.parse(xhr.responseText);
-                console.log('meddelande tillbaka: '+answer.message); //svarstext
                 
-                if(answer.nextURL != null){//RÄTT GRÖN FÄRG
-                    ansBox.innerHTML = answer.message;
-                    if(answer.nextURL === undefined){//om nexturl finns, skicka till GET
-                         console.log('det tog 300 försök du suger!');
-                         
-                     }else{//om inte skriv ut hur måna försök det tog
-                         
-                         console.log('gåt vidare URL finns');
-                         theQuiz.nextURL = answer.nextURL;
-                         theQuiz.get();
-                     }
+                if(xhr.readyState === 4){
+                    var ansBox = document.getElementById('answer');
+                    var answer = JSON.parse(xhr.responseText);
                     
-                }else{
-                    ansBox.innerHTML = answer.message;//fel BLI RÖD
-                    
+                    if(answer.nextURL != undefined){//RÄTT GRÖN FÄRG
+                        ansBox.innerHTML = answer.message;
+                        ansBox.setAttribute('style', 'border: 2px solid rgba(115, 240, 44, 0.5)');
+                        
+                        theQuiz.nextURL = answer.nextURL;
+                        theQuiz.get();
+                    }else if(xhr.status === 400){//fel BLI RÖD
+                        ansBox.innerHTML = answer.message;
+                        ansBox.setAttribute('style', 'border: 2px solid rgba(255, 44, 44, 0.5)');
+                        
+                    }else{//slut på frågor
+                        ansBox.setAttribute('style', 'border: 2px solid #8B6B45');
+                        ansBox.innerHTML = 'GRATTIS!<br>';
+                        theQuiz.timedArr.push(theQuiz.times);
+                        for(var i = 1; i <= theQuiz.timedArr.length; i++){
+                            ansBox.innerHTML += 'Fråga '+i+' tog '+theQuiz.timedArr[i-1]+' försök<br>';
+                        }
+                    }
+                    document.getElementById('textvalue').value = "";
                 }
-                document.getElementById('textvalue').value = "";
+                    
             };
     }
 };
